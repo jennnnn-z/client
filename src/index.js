db445f0fdefc444ce6e3f6ccbfe331ff32ffa3ab
Jennifer Zhang <jlz2@andrew.cmu.edu>
2024-07-23 16:08:10 -0700

    style changes

--- src/index.js
+++ src/index.js
@@ -1,15 +1,23 @@
-const url = "https://fullstackfinalproject-b2gn.onrender.com/";//"http://localhost:3005/";
+const url = "https://fullstackfinalproject-b2gn.onrender.com/"; //"http://localhost:3005/";
 
 var activities = [];
-$(document).ready(function(){
+$(document).ready(function () {
   // getAllFaves();
-  $("#getactivity").click(function(){
-    $.getJSON(url + 'activity?type=' + type.value, (thing) => {
+  $("#getactivity").click(function () {
+    $.getJSON(url + "activity?type=" + type.value, (thing) => {
       $("#activity").empty();
       console.log("got activity");
-      $("#activity").append("<p style=\"display:inline;\">You can "+ thing.activity.toLowerCase() + "! </p>");
-      $("#activity").append("<button class=\"tofavorite\" id=\""+ thing.key + "\">Click to Favorite</button>");
-      $("body").on('click', '#' + thing.key, (t)=>{
+      $("#activity").append(
+        '<p style="display:inline;">You can ' +
+          thing.activity.toLowerCase() +
+          "! </p>"
+      );
+      $("#activity").append(
+        '<button class="tofavorite" id="' +
+          thing.key +
+          '">Click to Favorite</button>'
+      );
+      $("body").on("click", "#" + thing.key, (t) => {
         console.log("calling POST on " + t.target.id);
         addFave(t.target.id);
         // remove button after clicked
@@ -17,33 +25,43 @@ $(document).ready(function(){
       });
     });
   });
-  
-  $("#refresh").click(function(){
+
+  $("#refresh").click(function () {
     // $("#allfaves").toggle();
     getAllFaves();
   });
-
-  
 });
 
-function getAllFaves(){
-  console.log('getting all faves...');
+function getAllFaves() {
+  console.log("getting all faves...");
   $("#allfaves").empty();
   $("#allfaves").append("<h2>Favorite Activities</h2>");
-  $.getJSON(url + 'favorites/', (faves) => {
+  $.getJSON(url + "favorites/", (faves) => {
     console.log(faves);
     faves.forEach((fave) => {
-      $("#allfaves").append("<div style=\"display:inline;\" id=\""+ fave.key + "\">" + fave.activity + "&nbsp;</div>");
+      $("#allfaves").append(
+        '<div style="display:inline;" id="' +
+          fave.key +
+          '">' +
+          fave.activity +
+          "&nbsp;</div>"
+      );
       // $("#allfaves").append("<p><code>data-ma-enter='submit'</code><span data-malleable='true' data-ma-blur='submit'>" + fave.activity + "</span></p>");
       // $("#allfaves").append("<a href=\"#\" id=\"username\" data-type=\"text\" data-pk="1" data-title=\"Enter username\" class=\"editable editable-click\" style=\"display: inline; background-color: rgba(0, 0, 0, 0);\">superuse</a>");
-      $("div#" + fave.key).append("<button class=\"edit\" id=\""+ fave.key + "\">Edit</button>&nbsp;");
-      $("div#" + fave.key).append("<button class=\"delete\" id=\""+ fave.key + "\">Delete</button><br>");
-      $("body").on('click', 'button#' + fave.key + ".delete", (t)=>{
+      $("div#" + fave.key).append(
+        '<button class="edit" id="' + fave.key + '">Edit</button>&nbsp;'
+      );
+      $("div#" + fave.key).append(
+        '<button class="delete" id="' + fave.key + '">Delete</button><br>'
+      );
+      $("body").on("click", "button#" + fave.key + ".delete", (t) => {
         console.log("calling DELETE on " + t.currentTarget.id);
         deleteThing(t.currentTarget.id);
       });
-      $("body").on('click', 'button#' + fave.key + ".edit", (t)=>{
-        console.log("calling PUT on " + t.currentTarget.id + ": " + fave.activity);
+      $("body").on("click", "button#" + fave.key + ".edit", (t) => {
+        console.log(
+          "calling PUT on " + t.currentTarget.id + ": " + fave.activity
+        );
         let newActivity = prompt("Enter new activity", fave.activity);
         console.log(newActivity);
         editFave(t.currentTarget.id, newActivity);
@@ -58,54 +76,52 @@ function getAllFaves(){
       //     editFave(t.target.id, t.target.innerText);
       //   }
       // });
-      });
+    });
   });
 }
 
-function addFave(key){
+function addFave(key) {
   // key = key.slice(3, key.length);
   console.log(key);
   $.ajax({
-    url: url + 'favorite/post/' + key,
-    type: 'POST',
+    url: url + "favorite/post/" + key,
+    type: "POST",
     success: (res) => {
-      console.log('Success', res);
+      console.log("Success", res);
       // getAllFaves();
-    }, 
+    },
     error: () => {
       // alert('Already Favorited!');
-    }
-  })
+    },
+  });
 }
 
-function deleteThing(key){
+function deleteThing(key) {
   $.ajax({
-    url: url + 'favorite/delete/' + key,
-    type: 'DELETE',
+    url: url + "favorite/delete/" + key,
+    type: "DELETE",
     success: (res) => {
-      console.log('Success', res);
-      // remove all items with the id of key 
+      console.log("Success", res);
+      // remove all items with the id of key
       $("div").remove("#" + key);
-    }, 
+    },
     error: () => {
-      alert('Already Deleted!');
-    }
-  })
+      alert("Already Deleted!");
+    },
+  });
 }
 
-
-function editFave(key, activity){
-  console.log('editing fave...' + key + ": " + activity);
+function editFave(key, activity) {
+  console.log("editing fave..." + key + ": " + activity);
   const body = {
-    'key': key,
-    'activity': activity
+    key: key,
+    activity: activity,
   };
   $.ajax({
-    url: url + 'favorite/update/',
-    type: 'PUT',
+    url: url + "favorite/update/",
+    type: "PUT",
     data: JSON.stringify(body),
-    dataType: 'json',
-    contentType: 'application/json',
-  })
+    dataType: "json",
+    contentType: "application/json",
+  });
 }
-
